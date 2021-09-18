bb3a4297e73cfc71b7069649fc2786dc17824f0a
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-09-18 07:38:08 +0500

    delete old files
    old django files

--- main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from djang.contrib.auth.models import User
-# Create your models here.
-
-class MyUser(User):
-	name = models.CharField('First name', max_length=50, blank=True, null=True)
-	username = models.CharField('Username', max_length=50, unique=True)
-	password1 = models.CharField('Passowrd 1', max_length=50)
-	password2 = models.CharField('Passowrd 2', max_length=50)
--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views 
-
-app_name = 'main'
-
-urlpatterns = [
-	path('', views.home, name='home'),
-	path('register/', views.register, name='register'),
-]
