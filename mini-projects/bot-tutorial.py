8d2b8e4d4161a8a378ffa3dce7cd81681fcbea70
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-11-30 18:39:00 +0500

    update bot-tutorial.py

--- mini-projects/bot-tutorial.py
+++ mini-projects/bot-tutorial.py
@@ -6,13 +6,3 @@
 # print(mean([1,2,3]))
 
 
-
-def mean(arr):
-    count = 0
-    nums = 0
-    for i in arr:
-        nums += i
-        count += 1
-    result  = nums / count
-    return result
-print(mean([1,2,3]))
