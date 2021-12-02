a8f3f1e19e863623923db13dce8d8a4ac2e21abb
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-12-02 09:34:24 +0500

    instagram parser

--- mini-projects/bot-tutorial.py
+++ mini-projects/bot-tutorial.py
@@ -5,4 +5,31 @@
 #     return(mu)
 # print(mean([1,2,3]))
 
+from instagramy import InstagramUser
+
+# Connecting the profile
+user = InstagramUser("Rashidov21")
+
+# printing the basic details like
+# followers, following, bio
+# print(dir(user))
+# print(user.fullname)
+# print(user.url)
+# print(user.username)
+# print(type(user.posts[0]))
+# print(dir(user.posts[0]))
+print(user.posts[0].post_url)
+print(user.posts[0].caption)
+print(user.posts[0].likes)
+
+
+
+# return list of dicts
+# posts = user.get_posts_details()
+#
+# print('\n\nLikes', 'Comments')
+# for post in posts:
+#     likes = post["likes"]
+#     comments = post["comment"]
+#     print(likes, comments)
 
