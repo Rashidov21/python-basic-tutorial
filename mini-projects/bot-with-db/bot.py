21e3254d5cabb600d84c39b66cc4593f8e53023d
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-01-04 18:42:07 +0500

    update python basics

--- mini-projects/bot-with-db/bot.py
+++ mini-projects/bot-with-db/bot.py
@@ -1,9 +1,24 @@
 import logging
 import sqlite3
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
+from weatherAPI import getWeather
+
+
+
+
 logging.basicConfig(level=logging.INFO)
-TOKEN = ""
+TOKEN = "1995282608:AAHgCF249xJdHInqfX97zBQ-zbliTTGSZoQ"
 me = 0
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+btn = KeyboardButton('Weather !', callback_data='Tashkent')
+
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard.add(btn)
+
+
+
 # con = sqlite3.connect("users.db")
 # cur = con.cursor()
 #
@@ -22,9 +37,6 @@ me = 0
 #     con.close()
 
 
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
@@ -52,26 +64,12 @@ async def send_welcome(message: types.Message):
 
     # print(type(message))
     # print(dir(message))
-    await message.reply(f"Hi! - {fname + ' ' + lname}\nI'm EchoBot!\nPowered by aiogram.")
+    await message.reply(f"Hi! - {fname + ' ' + lname}\nI'm EchoBot!\nPowered by aiogram.",  reply_markup=keyboard)
 
 @dp.message_handler(commands=['get'])
 async def send_users(message: types.Message):
-    u = []
-    try:
-        con = sqlite3.connect("users.db")
-        cur = con.cursor()
-            sql = """\
-                    SELECT * FROM users;
-                """
-        for row in cur.execute(sql):
-            u.append(row)
-    except sqlite3.DatabaseError as er:
-        print(er)
-    else:
-        print(u)
-
-
-    await message.reply(f"All users")
+    data = getWeather("Tashkent")
+    await message.reply(f"Temp = {data['temp']} Status={data['status']}")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
