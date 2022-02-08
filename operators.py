c265003f8b939b72676d68bc6dd8e68cd00125aa
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-02-08 23:18:54 +0500

    update operators

--- operators.py
+++ operators.py
@@ -1,3 +1,16 @@
+import requests
+
+url = "https://forecast9.p.rapidapi.com/"
+
+headers = {
+    'x-rapidapi-host': "forecast9.p.rapidapi.com",
+    'x-rapidapi-key': "0202709d4emshbe87778ed9b4962p1f76cdjsn4a200e532763"
+    }
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
+
 #int , str , float, bool
 # < , > , == , != , in ,not in, is,is not, and ,or ,
 #print(10 > 5) # True
@@ -23,3 +36,25 @@
 # <, > ,<=, >= , == , != , is ,is not , in , not in, and , or
 # Mantiqiy operatorlar >> not , and , or
 
+# Hamma Apple desa Stiv JObsni hayoliga keltiradi aslida ham shundaymi ?
+
+# Stiv Voznyak Apple kompaniyasini 35 yil oldin tark etgan.
+
+# Ko'pchilik uchun kompaniya nomi Stiv Jobs bilan bog'liq.
+# Bilgan odamlar kompaniya ortida ikkita ajoyib Stiv turganini tushunishadi.
+
+# Voznyakning birinchi yirik ishi Apple I bo'ldi. Undan so'ng kompaniyaning yillik daromadi 775 000 dollardan 118 million dollargacha, ya'ni 152 baravarga yaxshi o'sgan, tijorat jihatidan muvaffaqiyatli bo'lgan Apple II bo'ldi.
+
+# Voznyak daho edi, lekin uning mahsulotga bo'lgan yondashuvi odatdagidan tubdan farq qilardi. U aytganidek, u har doim qurilmalar ochiq va hamma uchun yetarli, tushunarli bo'lishini xohlardi. U o'z ishlanmalarini ixlosmandlar hamjamiyatiga baham ko'rishni xohladi, shunda bu ixlosmandlar ularni yaxshiroq, mukammalroq qilishlari mumkin edi. Bu yondashuv Jobsning dunyoqarashiga zid edi.
+
+# Dastlab, xuddi shu Apple II sakkizta portga ega bo'lishi kerak edi, Jobs ularni ikkitaga qisqartirmoqchi edi. Ammo Voznyak turib oldi. Keyin Stivlar bahslashdi: agar kompyuter sotuvda muvaffaqiyatsizlikka uchraganida, Voznyak kompaniyani tark etgan bo'lardi. Tarix shuni ko'rsatadiki, Voznyak haq edi va kompaniyadan ketdi.
+
+# Uning uchun so'nggi tomchi Apple III bo'ldi. Voznyak samolyot halokatidan keyin tiklanishi sababli ikki yil davomida mahsulot ustida ishlay olmadi. Ammo u rivojlanishga qaytganida, qurilma qutqarib bo'lmaydigan darajada edi. U Apple so'nggi besh yil davomida noto'g'ri yo'nalishda ketayotganini va kompaniyadagi ishi uning tavqi la'nati ekanligini aytib kompaniyani tark etdi.
+
+# Yillar davomida his-tuyg'ular sovidi. Voznyak va Jobs do'st bo'lib qolishdi. Va bugun Voznyak o'zi yig'gan Apple I anakartlarini (plata) xursandchilik bilan dashatlab bermoqda.
+
+# Voznyak Apple I  boshida shaxsiy foydalanish uchun yig'gan edi lekin Jobs ularni tijorat maqsadida pullamoqchi 
+# bo'lgan , bu vaqt oralig'ida (70-yillar oxiri) 200 ta Apple I ishlab chiqilgan.
+
+# Ha aytgancha xozirgi kunga kelib Apple I kompanyaning eng qimmat qurilmalari qatorida turadi. 2014-yili Genri Ford muzeyi Apple I ni 905 ming AQSH dollariga sotib olgan , bu narx Apple I ni o'sha vaqdagi boshlang'ich narxidan 1400 barobar
+# qimmatroq. 
