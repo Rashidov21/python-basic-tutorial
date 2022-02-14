cfe38b8839bf748a87dde4e0133dffb754c82e69
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-02-14 14:54:07 +0500

    update parser,py

--- mini-projects/web-scrapper-with-db/parser.py
+++ mini-projects/web-scrapper-with-db/parser.py
@@ -8,25 +8,33 @@ HEADER = {
 url = "https://socratify.net/quotes/best"
 content_page = requests.get(url, headers=HEADER)
 
+obj = []
+
 
 def get_content(page):
     soup = BeautifulSoup(page.text, "html.parser")
-    content = soup.findAll("div", class_="b-list-quote2__item")
+    page_tag_content = soup.findAll("div", class_="b-list-quote2__item")
     # quotes = content.findAll("div", class_="b-list-quote2__item ")
-    obj = []
+    if len(page_tag_content) != 0:
+        obj = list()
+        def parsing_data():
+            for x in page_tag_content:
+                title = x.find("a", class_='b-list-quote2__item-text js-quote-text').text
+                author = x.find("div", class_="b-list-quote2__item-category").a.text
+                item = {
+                    "title": title.strip(),
+                    "author": author.strip()
+                }
+                obj.append(item)
 
-    for x in content:
-        title = x.find("a", class_='b-list-quote2__item-text js-quote-text').text
-        author = x.find("div", class_="b-list-quote2__item-category").a.text
-        item = {
-            "title": title.strip(),
-            "author": author.strip()
-        }
-        obj.append(item)
-    return obj
+        parsing_data()
+        return obj
 
 
-# get_content(content_page)
+object_list = get_content(content_page)
+
+
+print(object_list)
 
 
 def write_json():
@@ -36,7 +44,9 @@ def write_json():
 
 def read_json():
     with open("quotes.json", "r") as read_file:
-        print(dir(read_file))
+        data = json.loads(read_file.read())
+
+        print(len(data[0]))
 
 
 # write_json()
