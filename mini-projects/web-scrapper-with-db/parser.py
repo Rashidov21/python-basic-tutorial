380dbdde35c33a4563ed7fd7ca56f22d9b779c9f
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-02-14 14:26:11 +0500

    add json for quotes

--- mini-projects/web-scrapper-with-db/parser.py
+++ mini-projects/web-scrapper-with-db/parser.py
@@ -1,22 +1,43 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
 
+HEADER = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
 
 url = "https://socratify.net/quotes/best"
-page = requests.get(url,headers=HEADER)
-soup = BeautifulSoup(page.text, "html.parser")
-content = soup.findAll("div", class_="b-list-quote2__item")
-# quotes = content.findAll("div", class_="b-list-quote2__item ")
-obj = []
+content_page = requests.get(url, headers=HEADER)
 
-for x in content:
-    title = x.find("a",class_='b-list-quote2__item-text js-quote-text').text
-    author = x.find("div", class_="b-list-quote2__item-category").a.text
-    item = {
-        "title":title.strip(),
-        "author": author.strip()
-    }
-    obj.append(item)
 
+def get_content(page):
+    soup = BeautifulSoup(page.text, "html.parser")
+    content = soup.findAll("div", class_="b-list-quote2__item")
+    # quotes = content.findAll("div", class_="b-list-quote2__item ")
+    obj = []
+
+    for x in content:
+        title = x.find("a", class_='b-list-quote2__item-text js-quote-text').text
+        author = x.find("div", class_="b-list-quote2__item-category").a.text
+        item = {
+            "title": title.strip(),
+            "author": author.strip()
+        }
+        obj.append(item)
+    return obj
+
+
+# get_content(content_page)
+
+
+def write_json():
+    with open("quotes.json", "w") as write_file:
+        json.dump(obj, write_file)
+
+
+def read_json():
+    with open("quotes.json", "r") as read_file:
+        print(dir(read_file))
+
+
+# write_json()
+read_json()
