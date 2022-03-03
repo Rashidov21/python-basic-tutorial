44c22ce1b94da6355d52bc5325ef68c6b09c3918
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-03-03 20:41:53 +0500

    add 2 files

--- mini-projects/insta_parser.py
+++ mini-projects/insta_parser.py
@@ -1,5 +1,22 @@
-import requests
+import instaloader
 
-url = 'https://www.instagram.com/rashidov_21/'
-data = requests.get(url)
-print(data.content)
+
+bot = instaloader.Instaloader()
+# profile = instaloader.Profile.from_username(bot.context, 'Rashidov21')
+# print(dir(profile))
+# print(profile.get_profile_pic_url())
+
+# print("Username: ", profile.username)
+# print("User ID: ", profile.userid)
+# print("Number of Posts: ", profile.mediacount)
+# print("Followers: ", profile.followers)
+# print("Followees: ", profile.followees)
+# print("Bio: ", profile.biography,profile.external_url)
+
+profile = instaloader.Profile.from_username(bot.context, 'bek_maloy.me')
+# Get all posts in a generator object
+posts = profile.get_posts()
+
+# Iterate and download
+for index, post in enumerate(posts, 1):
+    bot.download_post(post, target=f"{profile.username}_{index}")
