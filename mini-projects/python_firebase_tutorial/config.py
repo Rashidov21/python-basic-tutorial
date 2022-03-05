7bf69e336a6067000b3bf953174d98e4395f5bd4
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-03-05 19:15:35 +0500

    Update config.py

--- mini-projects/python_firebase_tutorial/config.py
+++ mini-projects/python_firebase_tutorial/config.py
@@ -1,15 +1,7 @@
 import pyrebase
 
 firebase_config = {
-    'apiKey': "AIzaSyBfePZU1LMCmrwxFqLYXiQF_n132SdZhC4",
-    'authDomain': "fruitsbase.firebaseapp.com",
-    'projectId': "fruitsbase",
-    'storageBucket': "fruitsbase.appspot.com",
-    'messagingSenderId': "445383361070",
-    'appId': "1:445383361070:web:4210d09aabe8622619d5bf",
-    'measurementId': "G-QCV1XJRVHR",
-    # Add this line
-    'databaseURL':'https://fruitsbase-default-rtdb.firebaseio.com/'
+    # YOUR API DATA
 }
 firebase = pyrebase.initialize_app(firebase_config)
 # auth = firebase.auth()
