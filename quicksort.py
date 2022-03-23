c78a4101f087011b131576ad82693d5509fa898a
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-03-23 07:12:52 +0500

    Create quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,10 @@
+def quicksort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+
+        return quicksort(less) + [pivot] + quicksort(greater)
+print(quicksort([1,8,3,6,4,5,7,9,2]))
