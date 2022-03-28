50b14ce4c91c623d075a2fdc85525d61d37fdde4
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-03-28 15:20:07 +0500

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -22,4 +22,6 @@ print(quicksort([1,8,3,6,4,5,7,9,2]))
 # for i in range(len("alpha")*2,2*8):
 #     print(i)
 # print(*[x for x in [0,2,-1,-6,5] if x > 0])
-print(sum(range(2,5))* len("python"))
+# print(sum([x for x in range(-3, 6)]))
+# print([x for x in range(-3, 6)])
+print(list(range(5)))
