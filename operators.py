13abdc3094b5c1ed79556d0ded153382f92c2363
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-04-28 10:54:33 +0500

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 url = "https://www.pexels.com/ru-ru/search/python/"
-page = requests.get(url)
+headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
+}
+page = requests.get(url, headers=headers)
 soup = BeautifulSoup(page.text, "html.parser")
 print(soup.findAll("article", class_="photo-item"))
 images = "sss"
