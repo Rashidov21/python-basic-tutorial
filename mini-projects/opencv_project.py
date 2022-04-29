9d1959d8ba98c291270d80717ba3243f665dd617
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-04-29 17:31:38 +0500

    add new files

--- mini-projects/opencv_project.py
+++ mini-projects/opencv_project.py
@@ -3,4 +3,19 @@
 import csv
 import json
 
-csvFile = ''
+csv_file = 'movie_metadata.csv'
+json_file = 'data.json'
+
+data = {}
+
+with open(csv_file, encoding='utf-8') as file:
+    csvReader = csv.DictReader(file)
+    for rows in csvReader:
+        # print(row)
+        data_id = rows['movie_title']
+
+        data[data_id] = rows
+# for i in data.items():
+#     print(i)
+# with open(json_file, "a") as json_file_data:
+#     json_file_data.write(json.dumps(data, indent=4)) 
