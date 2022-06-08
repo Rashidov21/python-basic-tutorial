07e89551d02105cc6f92e28dfffb2e6ed9482c29
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-06-08 16:08:12 +0500

    delete old group codes

--- mini-projects/fonlinebot/main.py
+++
@@ -1,8 +0,0 @@
-from aiogram import executor
-
-from app import bot
-
-
-executor.start_polling(bot.dp,
-                       skip_updates=True,
-                       on_shutdown=bot.on_shutdown)
