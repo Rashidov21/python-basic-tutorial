bb9f2588470c38f4b21da8b978144a028a25214e
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-06-17 16:43:14 +0500

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,3 +1,4 @@
+from transliterate import translit, get_available_language_codes
 # STRINGS
 # s = "python"
 # n_str = str(123456)
@@ -61,3 +62,58 @@
 # join - massivni str qiladi
 # print("".join(long_str.splitlines()).split("\n"))
 # ['python ok js ok cpp ok']
+
+# s = "python"
+# print("python is better".title())  # Python Is Better
+# print(s.upper())  # PYTHON
+# print(s.lower())  # python
+# print("python is better".capitalize())  # Python is better
+
+# print(chr(1))  # bu raqam qabul qiladi va ascii boyicha belgini qaytaradi
+# print(ord("a"))  # bu belgini qabul qiladi va ascii boyicha tartib raqamini qaytaradi
+# s = "python"
+# long_str = "split - siz korsatgan belgi boyicha str ni elementlarga ajratadi"
+# # siz korsatgan matnni boshlanish index ini qaytaradi
+# print(long_str.find("siz"))  # 8
+# print(long_str.find("text"))  # -1
+# print(long_str.rfind("str"))  # 36
+# print(s.index("h"))  # 3
+# print(s.rindex("l"))  # ValueError
+# aks holda -1 qayataradi
+
+
+# s = "A byte of python"
+# ru_text = "лучшее место для поиска векторных изображений."
+# ['bg', 'el', 'hy', 'ka', 'l1', 'mk', 'mn', 'ru', 'sr', 'uk']
+
+# print(translit(s, "ru"))  # А быте оф пытхон
+# print(translit(ru_text, "mn"))  # А быте оф пытхон
+# print(s.count("o"))  # 2 | elementni str dagi sonini qaytaradi
+
+# print(s.startswith("A"))  # True
+# print(s.startswith("a"))  # False
+# print(s.startswith("a".lower()))  # Every OK
+
+##########
+# print(s.endswith("on"))  # True
+# print(s.endswith("of"))  # False
+
+########
+# print(s.replace("python", "javascript"))
+
+########
+# print("abc123".isalnum())  # True | harf yoki osnlar
+# print("abc123".isalpha())  # False | faqat harflar
+# print("123".isdigit())  # True | faqat butun sonlar
+# print("123".isdecimal())  # True | faqat  sonlar
+# print("123".isnumeric())  # True | faqat butun, kasr, rim , qoldiqli sonlar
+# print("str".isupper())  # False
+# print("str".islower())  # True
+# print("str".istitle())  # True
+
+
+# task 1
+# input: user son kiritadi agar  son - da bolmasa  yoki 0 ga teng bomasa
+# sonlarni yigindisini hisoblang
+# while orqali toxtovsiz son kiritiladi agar "stop" deb kiritlsa sikl toxtedi va sonlar
+# yigindisi ekranga chiqadi
