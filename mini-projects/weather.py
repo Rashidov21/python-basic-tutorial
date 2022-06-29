a69f609501ba571df860284201ba89df883765f0
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-06-29 19:19:20 +0500

    Update weather.py

--- mini-projects/weather.py
+++ mini-projects/weather.py
@@ -12,13 +12,16 @@ response = requests.get(url)
 # print(response.status_code)  # agar 200 bo'lsa demak OK
 # kelgan json ko'rinishidagi malumotlarni python dict ga olish
 data = response.json()
+for i in data.items():
+    print(i)
 
-
+country = data.get("sys").get("country")
 temp_f = data.get("main").get("temp")
 temp_c = round(temp_f - 273, 2)
 city_name = data.get("name")
 status = data.get("weather")[0].get("main")
 desc = data.get("weather")[0].get("description")
+print(Fore.RED + f"Country : {country}")
 print(Fore.RED + f"city : {city_name}")
 print(Fore.CYAN + f"Temperature F : {temp_f}")
 print(Fore.YELLOW + f"Temperature C : {temp_c}")
