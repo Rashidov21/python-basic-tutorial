ea85262bc9bfeb4971a2e194a8a9893db22530cc
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-07-04 18:32:37 +0500

    Create tasks.py

---
+++ tasks.py
@@ -0,0 +1,39 @@
+#  task 1
+#  random sonlardan (0, 999) iborat massiv minimum 20 ta sondan iborat
+#  agar son (0 , 99) orasida bosa uni kvadrati chiqadi
+#  agar son(99, 399) orasida bosa kubi chiqadi
+#  qolgan sonlarni o'zi console ga chiqdi
+
+# // task 2
+# letters = "abcdefghijklmnopqrstuvwxyz"
+# d = {
+#     "a":"01",
+#     "b":"02",
+#     ....
+# }
+# d = {}
+# for i in letters:
+#     val = ""
+#     if letters.index(i) <= 9:
+#         val = f"0{letters.index(i)+1}"
+#         d.update({f"{i}": val})
+#     else:
+#         d.update({f"{i}": letters.index(i)})
+# u = input()
+# r = ""
+# for x in u:
+#     if x in d.keys():
+#         r += f"{d[x]}"
+# print(r)
+# print("".join([str(x) for x in range(10)]))
+
+
+# task 3
+# // input: user >> "bed"
+# // output: >> 020403
+
+
+# // task 4
+# // Userdan 3 ta son qabul qiling ularni to'g'ri joylashtirish orqali
+# // sonlardan  eng katta qiymatni xosil qiling
+# // 1,7,3 >> 731
