b18b5139f56892ea98cbe7c236959f7c7763743f
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-07-25 08:06:37 +0500

    Update api_test.py

--- mini-projects/quran_bot/api_test.py
+++ mini-projects/quran_bot/api_test.py
@@ -1,4 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get("https://api.mp3quran.net/api/translations?languages=ru")
-print(r.json())
+# r = requests.get("http://api.mp3quran.net/api/surah?surah=1&language=ru")
+# r = requests.get("http://api.mp3quran.net/radios/radio_.json")
+
+page = requests.get("https://namaz.today/city/andizhan")
+data = BeautifulSoup(page.text, 'html.parser')
+print(data)
