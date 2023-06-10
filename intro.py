8f7f7fd98ba7e79504dab08a41b1d017544d5fbd
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-06-10 15:34:28 +0500

    Create intro.py
    first lesson on python

---
+++ intro.py
@@ -0,0 +1,92 @@
+"""
+
+Python Basic 3.8.8
+Seshanba 14:00 
+Tutorial file 
+
+"""
+# print("Hello world!")
+
+# python2 - print "Hello world !"
+# python3 - print("Hello world !")
+
+# comment line
+
+# print(input("name ?"))
+
+# Variables 
+
+# O'zgaruvchi - kompyuter xotirasidagi katakchalarga olib boruvchi manzil
+
+# def main(x,y):
+#     print(x,y)
+    
+# print(main) # <function main at 0x00000225A76361F0>
+# name = "John"
+
+# 1-mantiqan togri nom 
+# 2-ingliz tilida 
+# 3-birinchi belgi raqam bolishi mumkin emas
+# 4-faqat lotin harflari , sonlar va _ mumkin holos
+
+# userProfileImage = 'src/profile.jpg' # Camel Case
+# user_profile_image = 'src/profile.jpg' # Kebab Case
+
+# a = 10
+# b = 5
+# b = 2
+# print(a+b) # 12
+
+# x,y,z = 1,2,3
+# print(x+y+z) # 6
+
+
+
+# AUTH_USER = 'gandi111'
+# AUTH_USER = 'mahatma333'
+# print(AUTH_USER)
+# import keyword
+# print(keyword.kwlist)
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+
+# Data Types , Data Structure
+# bool - True/False 
+# print(bool(1)) # True
+# print(bool("text")) # True
+# print(bool("")) # False
+# print(bool(0)) # False
+
+# int - 1 ,-1
+# x = 10
+# y = -5
+# z = 2
+# print(x+y*z) # 0
+# print((x+y)*z) # 10
+
+# float - 1.0 , -1.0
+# x = 5
+# y = 5.3
+# print((x*2) - (x * y)) # -16.5
+
+# print(type(x)) # <class 'int'>
+# print(type(y)) # <class 'float'>
+# complex - 2n 
+
+
+# age = int(input('Your age ? \n')) # 20
+# weight = float(input("Weight ?")) # 78
+# # "20"*2 = "2020"
+# print(age * 2) # 40
+# print(weight) # 78.0
+
+# print(str(age)*5) # 2020202020
+
+# Data Types - bool,int,float,complex,str,None
+
+# Data Structure - list,tuple,dict,set,frozenset,bytes,bytearray
+
+
+# task 1
+# a = int(input("A ? "))
+# print("Perimetr =",4*a)
