70ac059aa08a5221aa8e39d28fd263077536216c
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-06-15 15:50:02 +0500

    update file

---
+++ if_conditions.py
@@ -0,0 +1,20 @@
+# if - agar
+# elif - agarda , aks holda yoki 
+# else - aks holda 
+
+# n = int(input())
+# if n > 0:
+#     if n % 2 == 0:
+#         print("Musbat Juft")
+#     else:
+#         print("Musbat Toq")
+# elif n == 0:
+#     print("bu 0 soni")
+# else:
+#     if n % 2 == 0:
+#         print("Manfiy Juft")
+#     else:
+#         print("Manfiy Toq")
+# print("1 > 0 ? '1 katta' : '0 katta'")
+# if_ternary = "soni" if n == 5 else "boshqa son" # True
+# print(f"{n} soni" if n == 5 else "boshqa son")
---
+++ loops.py
@@ -0,0 +1,28 @@
+# for loop 
+# range - diapazon , oraliq  
+# range(start,stop,step)
+# range(1,10,2) >> 3,5,7,9,11
+
+# print(range(1,10,2)) # range(1, 10, 2)
+# print(type(range(1,10,2))) # <class 'range'>
+
+# for i in range(10):print(i)
+
+# for i in range(1,10,2):
+#     print(i)
+# print(1)
+# print(2)
+# print(3)
+
+# for i in "text":
+#     print(i)
+#     if i == "t":
+#         print(i*2) # IndentationError
+# else:
+#     print("for loop end")
+
+# k = 5
+# n = int(input("N"))
+# if n > 0:
+#     for i in range(n):
+#         print(k)
