61cbec1f4e0f8118bf38d6f7a79ff5155541be00
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-06-27 15:37:53 +0500

    Create tasks.py

---
+++ tasks.py
@@ -0,0 +1,40 @@
+# task 1 
+# Foydalanuvchidan matn qabul qiling , unda "gaz" , "svet", "yo'q" kabi so'zlar ishtirok etganmi yoki yo'qmi aniqlang va ular sonini har birini alohida hisoblab boring so'zlar register turi yoki boshqa qoshimchalar bilan kelishi mumkin bularni ham inobatga oling
+
+# Gaz yo'q ekan lekin svet bor svetamuzika
+
+# gaz = 1
+# svet = 2
+# text = "Gaz yo'q ekan lekin svet bor svetamuzika gazini bos"
+# gaz = 0
+# svet = 0
+# no = 0
+# for t in text.split(" "):
+#     if t.lower() == "gaz" or "gaz" in t.lower():
+#         gaz += 1
+#     if t.lower() == "svet" or "svet" in t.lower():
+#         svet += 1
+#     if t.lower() == "yo'q":
+#         no += 1
+# print(f"\ngaz={gaz}\nsvet={svet}\nno={no}")
+
+# task 2 
+# quyidagi shartlarni tekshirib parol togri yoki notogriligini aniqlaydigan dastur tuzing
+# password:
+#     - minimum 6 ta belgi 
+#     - kamida bitta katta harf 
+#     - kamida bitta maxsus belgi (/,\,@,/,_,-)
+
+# v = "Salom#Qalesan#Alik#"
+# l = len(v)
+card = "8600 1600 2530 1535"
+c = 0
+result = ""
+
+for n in card.split(" "):
+    if c == 3:
+        result += n 
+    else:
+        result += "**** "
+        c += 1
+print(result)
