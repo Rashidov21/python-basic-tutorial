a8941b4f549a56febafb7ab368a0dcc77723a0ca
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-07-07 14:03:11 +0500

    add

---
+++ bootcamp/data_types.py
@@ -0,0 +1,47 @@
+# Data type - ma'lumot turi
+# str - String , qator - matn
+# int - Integer , butun son 
+# float - Float , qoldiqli son
+# complex - qiyin , katta sonlar
+# bool - Boolean , rost yoki yolgon qiymatlari
+# None - None , mavjud emas qiymati
+
+
+# name = "John"
+# name = 'Mike'
+# name = """David"""
+# print(name) #David
+
+# print('Men bugun olma "yedim".') # Men bugun olma "yedim".
+# print("Hello\nWorld") # yangi qatordan
+# print("Hello\tWorld") # Tab 4 ta bosh joy tashash
+# print("Men bugun olma \"yedim\".") # Men bugun olma "yedim".
+
+# a,b,c = 1,2,3
+# print(a+b+c) #6 
+# x = False
+# print(type(x)) # <class 'bool'>
+# x = 1
+# print(type(x)) # <class 'int'>
+
+# user = input("Son kirit") # userdan ma'lumot qabul qilish
+# user = int(user) # stringni int qilish
+# print(user + 2)
+# # print(type(user))
+
+# user = int(input("Son kiriting \n:"))
+# print(user + 2)
+
+# # Arifmetika 
+# print(2 + 2) # 4
+# print(2 - 2) # 0
+# print(2 * 2) # 4
+# print(4 / 2) # 2.0
+# print(4 // 2) # 2
+# print(2 ** 4) # 16
+# print(7 % 3) # 1
+
+
+# task 1 
+a = int(input("KV tomoni "))
+print("Perimetr=", 4*a)
---
+++ bootcamp/intro.py
@@ -0,0 +1,51 @@
+
+# def main():
+#     """this is function ...."""
+    
+# """""" - faqat 1 qatorda yoki class , function lar bayoni uchun ishlatiladi  
+# comment - bir qatorlik komentlar
+
+# CTRL + / 
+
+# PEP-8  - Pythonda kod yozish konistitutsiyasi
+
+# help() - bu yordam 
+# print(help(input)) 
+"""
+input(prompt='', /)
+    Read a string from standard input.  The trailing newline is stripped.
+    
+    The prompt string, if given, is printed to standard output without a
+    trailing newline before reading input.
+    
+    If the user hits EOF (*nix: Ctrl-D, Windows: Ctrl-Z+Return), raise EOFError.
+    On *nix systems, readline is used if available.
+"""
+
+# O'zgaruvchi deb xotira kakatchasiga olib boruvchi manzilga aytiladi
+
+# n = 10
+# print(n + 2) # 12
+
+# number = 5
+
+# 1. Mantiqan to'g'ri nom 
+# 2. Ingliz tilida (maxsus belgilardan faqat _ mumkin)
+# 3. Birinchi belgi raqam bo'lishi mumkin emas 
+# 4. Kichkina harflar bilan
+# 5. Uzundan uzun nom bo'lishi mumkin emas 
+
+
+# gsn = "Abdulaziz"
+# groupstudentname = "Abdulaziz"
+# groupStudentName = "Abdulaziz" # Camel Case 
+# group_student_name = "Abdulaziz" # Kebab Case *
+
+# import keyword
+# print(keyword.kwlist)
+
+# # ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+# _not = 20
+# print(_not) # 20
+
