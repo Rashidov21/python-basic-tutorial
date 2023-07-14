b1dc2a84070d523bb010b1c32cded2880f5944e7
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-07-14 14:21:18 +0500

    Create loops.py

---
+++ bootcamp/loops.py
@@ -0,0 +1,33 @@
+# for , while - takrorlanish
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# for i in "12345":
+#     print(i)
+    
+# range - oraliq , diapazon
+# print(range(5)) # 0 dan 5 gacha bo'lgan sonlar oralig'i , ketam-ketlik
+
+# for k in range(5):print(k)
+
+# range(start,stop, step)
+
+# for x in range(1,11):
+#     print(x)
+
+# for i in range(5):
+#     x = int(input())
+#     print(x ** 2)
+    
+# name = input("Your name ?\n:")
+# count = 0
+# for i in name:
+#     if i.lower() == "a":
+#         count += 1
+# print("A letter count = ", count)
+
+# task 1 
+# 1 dan 100 gacha bo'lgan sonlar orasida 5 ga qoldiqsiz bo'linadigan  barcha sonlarni ekranga chiqaring
