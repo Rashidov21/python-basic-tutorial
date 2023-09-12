6395078c0913c0bbbad81693e514436d02580e8d
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-09-12 17:41:26 +0500

    Update intro.py

--- zero_coding/intro.py
+++ zero_coding/intro.py
@@ -40,14 +40,39 @@ q = lambda l: q([x for x in l[1:] if x <= l[0]]) + [l[0]] + q([x for x in l if x
 # print(list(filter(lambda x: x != 0,[func(x,y) for x in range(1,6) for y in list(range(1,6))[::-1]])))
 
 # trick 2 
-text = '''
-Call me Ishmael. Some years ago - never mind how long precisely - having
-little or no money in my purse, and nothing particular to interest me
-on shore, I thought I would sail about a little and see the watery part
-of the world. It is a way I have of driving off the spleen, and regulating
-the circulation. - Moby Dick'''
+# text = '''
+# Call me Ishmael. Some years ago - never mind how long precisely - having
+# little or no money in my purse, and nothing particular to interest me
+# on shore, I thought I would sail about a little and see the watery part
+# of the world. It is a way I have of driving off the spleen, and regulating
+# the circulation. - Moby Dick'''
+# print(text[9:100])
+# t = "tango"
+# print(t.replace("t","m")) #mango
+# print("m"+t[1:]) # mango
+# print()
+
 
 # matnda har bir qatorda 3 ta belgidan kop bolgan so'zlardan iborat massiv tuzing 
 
 # TASK 2
 # shu yozayotgan faylni ochish undagi keyinga qatorga o'tish belgilarini va bosh joylar sonini sanash
+
+
+# bytes - hom , bit ko'rinishidagi belgilar ketma-ketligi 
+# str - bu o'zida unicode belgilarni saqlaydi
+# unicode - belgilarni bayt ya'ni 8 ta bit ko'rinishida saqlanishi , (utf-8) 
+
+# x = "1"
+# print("True" if x else "False") 
+
+# if x:
+#     print("True")
+# else:
+#     print("false")
+
+# task 
+matrix = [ [ 1, 2, 3 ] , [4, 5, 6 ] , [7, 8, 9 ] ]
+# massiv ichidagi har bir son uchun lambda funksiya qollang funksiya agar son toq bolsa uni darajasini aks holda kubini qaytarsin, massivni o'zgartrib ekaranga chiqaring . faqat list generator orqali qiling
+print([x**2 if x % 2 == 0 else x ** 3 for i in matrix for x in i ])
+print(list(map(lambda x: x**2 if x % 2 == 0 else x ** 3, [x for i in matrix for x in i])))
